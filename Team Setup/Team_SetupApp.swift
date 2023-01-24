90b9f71204443e5ac9091d04f5494b04ebcc4c22
William Hatcher <william@hatcher.work>
2023-01-23 19:18:28 -0600

    Initial Commit

---
+++ Team Setup/Team_SetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team_SetupApp.swift
+//  Team Setup
+//
+//  Created by William Hatcher on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Team_SetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
